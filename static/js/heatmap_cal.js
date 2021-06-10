e9a5429fb908b26fe80f5c46215ded0756b51da7
Reuben Suju Varghese <reubensuju@gmail.com>
2021-06-10 16:36:25 +0300

    Stylized the input file button

--- static/js/heatmap_cal.js
+++ static/js/heatmap_cal.js
@@ -1,10 +1,10 @@
 var cal = new CalHeatMap();
 	cal.init({
-    data: "datas-years.json",
+    data: "dataFiles/datas-years.json",
 	start: new Date(2000, 0, 1),
 	id : "graph_c",
 	domain : "month",			// Group data by month
 	subDomain : "day",			// Split each month by days
 	range : 12,					// Just display 3 months
-	scale: [20, 40, 60, 80] 	// Custom threshold for the scale
+	legend: [20, 40, 60, 80] 	// Custom threshold for the scale
   });
